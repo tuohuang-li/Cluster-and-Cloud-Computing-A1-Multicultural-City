5ca80f867c1da7475b317130beeb5b601d7c574b
Tuohuang Li <tuohuangl@student.unimelb.edu.au>
2022-04-01 19:21:48 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,186 @@
+import json
+import os
+import pandas as pd
+import numpy as np
+
+
+os.chdir(r'C:\Users\Tuohuang\Documents\COMP90024-Cluster and cloud computing\A1\a1-code')
+#file = open('sydGrid-2.json')
+#sydGrid = json.load(file)  # type = dict
+#print (type(sydGrid))
+
+#print(len(sydGrid["features"][0]['geometry']['coordinates'][0]))
+
+"""
+To solve the following issue -
+UnicodeDecodeError: 'charmap' codec can't decode byte 0x81 in position 
+31510: character maps to <undefined>. 
+have to add"encoding="utf8" when open the file
+
+
+#file_tiny = open('tinyTwitter.json', encoding="utf8")
+#tinyTwitter = json.load(file_tiny)
+
+#for key in tinyTwitter['rows'][0]['doc']['coordinates'].keys():
+
+#    print(key)
+"""
+
+"""
+step 1: find coordinates
+"""
+def loadFile(file):
+    with open(file, 'r', encoding="utf8") as f:
+        gridData = json.load(f)
+    return gridData
+
+def getCoordList(grid):
+    """
+    :param grid: dictionary obtained via open file
+    :return: two list stores statistics of the top left corner coordinates (latitude/longitude respectively)
+    """
+    long = []
+    lat = []
+    for item in grid["features"]:
+        top_l_point = item["geometry"]["coordinates"][0][0]
+        #bottom_r_point = item["geometry"]["coordinates"][0][2]
+        long.append(top_l_point[0])
+        lat.append(top_l_point[1])
+
+    longitude = sorted(set(long))
+    latitude = sorted(set(lat), reverse=True)
+
+    return latitude, longitude
+
+"""
+Step 2: Assign Area code
+"""
+
+def getGrid(grid, latList, longList):
+    """
+    x1: longitude of the top left point eg.150.7655
+    y1: latitude of the top left point eg. -33.55412
+    x2: longitude of the bottom right point
+    y2: latitude of the bottom right point
+    LatCode: A,B,C,D
+    LongCode: 1,2,3,4
+    :param grid: A dictionary read from grid json file.
+    :return: a pandas dataframe contains grid ID, two points.
+    """
+    global latCode, longCode
+    codeMap = {0:'A', 1:'B', 2:'C', 3:'D'}
+    grid_info_df = pd.DataFrame(columns=['id', 'latCode','longCode', 'x1', 'y1', 'x2', 'y2'])
+
+    for item in grid["features"]:
+        properties = item["properties"]
+        top_l_point = item["geometry"]["coordinates"][0][0]
+        bottom_r_point = item["geometry"]["coordinates"][0][2]
+        id = properties["id"]
+        x1 = top_l_point[0]
+        y1 = top_l_point[1]
+        x2 = bottom_r_point[0]
+        y2 = bottom_r_point[1]
+
+        for i in range(len(latList)):
+            if top_l_point[1] == latList[i]:
+                latCode = codeMap[i]
+
+        for j in range(len(longList)):
+            if top_l_point[0] == longList[j]:
+                longCode = j+1
+
+        polygon_df = pd.DataFrame([[id, latCode, longCode, x1, y1, x2, y2]],
+                                  columns=['id', 'latCode' ,'longCode', 'x1', 'y1', 'x2', 'y2'], dtype='float')
+        grid_info_df = grid_info_df.append(polygon_df, ignore_index=True)
+
+    grid_info_df = grid_info_df.astype({'id': 'int64', 'longCode':'int64'})
+    grid_info_df = grid_info_df.convert_dtypes()
+    print(grid_info_df.info())
+    grid_info_df["cell"] = grid_info_df["latCode"] + grid_info_df["longCode"].astype(str)
+    grid_info_df = grid_info_df.set_index("cell")
+    grid_info_df = grid_info_df.drop(["id", "latCode", "longCode"], axis=1)
+
+    return grid_info_df
+
+
+sydGrid = loadFile('sydGrid-2.json')
+lat, long = getCoordList(sydGrid)
+df = getGrid(sydGrid, lat, long)
+print(df)
+#print(list(df['x1']))
+
+
+
+"""
+Step 3: Read twitter file
+"""
+
+twitts_dict = loadFile('tinyTwitter.json')
+
+
+def process_twitts (twitts):
+    """
+    Write a loop iterate through all data and discard items without geo/coordinates information
+    then store in pandas dataframe
+    :param twitts: A dictionary read from twitter json file.
+    :return: a pandas dataframe contains language code, coordinates and cell info
+    """
+
+    # Define a dataframe
+    twitts_info_df = pd.DataFrame(columns=['lang_code', 'cell', 'x', 'y'])
+    for item in twitts["rows"]:
+        if item["doc"]["coordinates"] is not None:
+            #print (item)
+            #print (item["doc"]["coordinates"]["coordinates"])    # type: list
+            lang_code = item["doc"]["lang"]
+            x = item["doc"]["coordinates"]["coordinates"][0]
+            y = item["doc"]["coordinates"]["coordinates"][1]
+            #cell =
+            """
+            need a function to decide the "cell" of this twitter
+            """
+
+            itemInfo_df = pd.DataFrame([[lang_code, x, y]],
+                                      columns=['lang_code', 'x', 'y'], dtype='float')
+            twitts_info_df = twitts_info_df.append(itemInfo_df, ignore_index=True)
+
+    #print(twitts_info_df)
+    return twitts_info_df
+
+process_twitts(twitts_dict)
+
+#t['combined']= t.values.tolist()
+dictionaryObject = df.to_dict(orient='index')  #index 0-15
+print(dictionaryObject)
+
+def cell_allocator(x, y, grid):
+    """
+    If a tweet occurs right on the border of two cells, keep left, keep down;
+    If located on edge border, for example A1, then it can be regarded as being in cell A1.
+    :param x: longitude of twitter sent
+    :param y: latitude of twitter sent
+    :param grid: grid dataframe contains 16 grids' information
+    :return: cell code eg. A1, C4, D2
+    """
+    dictionaryGrid = grid.to_dict(orient='index')  #{'C4': {'x1': 151.2155, 'y1': -33.85412, 'x2': 151.3655, 'y2': -34.00412}...}
+    #situation 1: not on any line
+
+
+    #return cell
+    pass
+
+
+
+class pair:
+    def __init__(self, key, value):
+        pass
+
+class grid:
+    def __init__(self,top_left,bottom_right):
+        pass
+
+    def isInGrid(input):
+        pass
+     # return Ture/False
+
+
